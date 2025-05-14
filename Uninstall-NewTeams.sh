a38480014e6bcfd114ff19dae166f208738a6521
The Knox Tech <119535757+theknoxtech@users.noreply.github.com>
2025-05-14 10:25:40 -0400

    Delete loggedinuser.sh Add final version Uninstall-NewTeams.sh

--- Uninstall-NewTeams.sh
+++ Uninstall-NewTeams.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-# Quit Teams
-pkill -9 -f "Microsoft Teams"
-
 # Check for Teams app
 if [ -d "/Applications/Microsoft Teams.app" ]; then
 
     echo "Quitting Teams"
-    pkill -9 -f "Microsoft Teams"
+    osascript -e 'quit app "Microsoft Teams"'
 
     echo "Uninstalling Teams"
     sudo rm -rf "/Applications/Microsoft Teams.app"
 
+    if [ ! -d "/Applications/Microsoft Teams.app" ]; then
+
+      echo "SUCCESS: Teams has been uninstalled"
+    else
+      echo "FAILURE: Teams has NOT been uninstalled"
+    fi  
+
 else
-  echo "App is not installed"
+  echo "Teams was not found on the target Mac"
 fi
-sudo rm -rf "/Applications/Microsoft Teams.app"
